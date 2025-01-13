92e6a698b479fc977f6d5c35b71f6a54fa02f3c4
moonlight4617 <moonlight4617@gmail.com>
2025-01-13 07:17:40 +0000

    middlewareにconsole

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -4,11 +4,12 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const accessToken = req.cookies.get("access_token");
+  console.log("Access token:", accessToken);
 
   // ログインが必要なページへのアクセスを保護
-  // if (pathname.startsWith("/content") && !accessToken) {
-  //   return NextResponse.redirect(new URL("/", req.url));
-  // }
+  if (pathname.startsWith("/content") && !accessToken) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
   // ログイン済みのユーザーがログインページにアクセスした場合はリダイレクト
   if (pathname === "/" && accessToken) {
